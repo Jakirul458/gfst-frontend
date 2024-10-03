2dffb615d39ee10772542366ffeee488ada967fa
Jakirul458 <rimon458jacky@gmail.com>
2024-10-03 21:30:36 +0530

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -51,41 +51,14 @@
 
 
 
-// import React from 'react'; 
-// import { NavLink } from 'react-router-dom';
-// import './Navbar.css'; // Import the CSS file
-
-// function Navbar({ handleLogout }) {
-//   return (
-//     <div className="navbar-container">
-//       <div className="navbar-brand">
-//         <div className="heading">Youth Supportive Society</div>
-//       </div>
-
-//       <div className="navbar-links">
-//         <NavLink to='/app/society/adminpanel' className="nav-link">Admin Panel</NavLink>
-//         <NavLink to='/app/society/dashboard' className="nav-link">Dashboard</NavLink>
-//         <NavLink to='/app/society/about' className="nav-link">About</NavLink>
-//         <NavLink to='/app/society/contact' className="nav-link">Contact</NavLink>
-//         <button onClick={handleLogout} className="nav-link logout-button">Logout</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-
 import React from 'react'; 
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
-import logo from "../../store"; 
+import './Navbar.css'; // Import the CSS file
 
 function Navbar({ handleLogout }) {
   return (
     <div className="navbar-container">
       <div className="navbar-brand">
-        
-        <img src={logo} alt="logo" className="navbar-logo" />
         <div className="heading">Youth Supportive Society</div>
       </div>
 
@@ -101,4 +74,3 @@ function Navbar({ handleLogout }) {
 }
 
 export default Navbar;
-
