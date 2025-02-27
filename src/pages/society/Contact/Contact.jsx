4c746a6e7fdca896d1368d2abe3cf975c19a3f7a
Jakirul458 <rimon458jacky@gmail.com>
2025-02-27 15:47:25 +0530

    nothing

--- src/pages/society/Contact/Contact.jsx
+++ src/pages/society/Contact/Contact.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import './Contact.css';  // Import the CSS file for styling
+import './Contact.css';  
+import logo from '../../../assets/icons/logo.svg';
 
 function Contact() {
     return (
         <div className="contact-container">
+            <img src={logo} alt="GFST Logo" className="logo" />
             <h2 className="contact-title">Contact Information</h2>
             <p className="contact-description">
                 If you have any questions or need further information, feel free to contact us:
             </p>
             <div className="contact-info">
-                <a href="mailto:gfcsmsd@gmail.com" className="contact-link">
+                <a href="mailto:gfcsmsd@gmail.com?subject=Inquiry&body=Hello," className="contact-link">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" alt="Gmail Logo" className="contact-icon" />
-                    gfcsmsd@gmail.com
+                    <span className="gmail-text">gfcsmsd@gmail.com</span>
                 </a>
-                <a href="mailto:jakirul458rimon@gmail.com" className="contact-link">
+                <a href="mailto:jakirul458rimon@gmail.com?subject=Inquiry&body=Hello," className="contact-link">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" alt="Gmail Logo" className="contact-icon" />
-                    jakirul458rimon@gmail.com
+                    <span className="gmail-text">jakirul458rimon@gmail.com</span>
                 </a>
-                <span className="contact-phone"> Sarifur Rahaman  +91 9733542533</span>
-                <span className="contact-phone">Ayub Sk +91 7029121433 </span>
-                <span className="contact-phone">Ripon Sk +91 8337840226 </span>
-                <span className="contact-phone">Jakirul Sk +91 6294527072</span>
+                <span className="contact-phone">Sarifur Rahaman  +91 9733542533</span>
+                <span className="contact-phone">Ayub Sk +91 7029121433</span>
+                <span className="contact-phone">Ripon Sk +91 8337840226</span>
+                
             </div>
         </div>
     );
